246b710dbbe0b8fc151fe004cee62c8651995883
Dominic Owusu Abeyie <83135226+dowusuabeyie@users.noreply.github.com>
2025-10-24 11:23:49 +0000

    revised

--- diag_targetline_small_dim_plot.py
+++ diag_targetline_small_dim_plot.py
@@ -152,12 +152,12 @@ plot_scatter(xs_ret, ys_ret,
              xlim=(150000, 260000), ylim=(150000, 260000))
 
 plot_scatter(xs_contr_amt, ys_contr_amt,
-             "Contribution (macrodata)", "Contribution (microdata)",
+             "Contributions (macrodata)", "Contributions (microdata)",
              "tab:green", "targetline-contr-amt",
              xlim=(1000000000, 9000000000), ylim=(1000000000, 9000000000))
 
 plot_scatter(xs_ben_amt, ys_ben_amt,
-             "Benefit (macrodata)", "Benefit (microdata)",
+             "Benefits (macrodata)", "Benefits (microdata)",
              "tab:red", "targetline-ben-amt",
              xlim=(1000000000, 7000000000), ylim=(1000000000, 7000000000))
 
